5b7f1fc2d4a70d47bde4ee395b39a149c328ac23
Soukalin Ghosh <97680241+soukalin96@users.noreply.github.com>
2022-01-15 19:54:16 +0530

    Update gate-switch models.v

--- gate-switch models.v
+++ gate-switch models.v
@@ -67,6 +67,86 @@ module tb;
   
   initial #100 $stop;
   endmodule
-//___________
 
 
+//3 in AND gate  
+
+module and3 (out,a,b,c);
+  output out;
+  input a,b,c;
+  supply0 gnd;
+  supply1 vdd;
+  wire w1,w2,y;
+  
+  pmos p1(y,vdd,a);
+  pmos p2(y,vdd,b);
+  pmos p3(y,vdd,c);
+  
+  nmos n1(y,w1,a);
+  nmos n2(w1,w2,b);
+  nmos n3(w2,gnd,c);
+	
+	pmos  (out, vdd, y);
+	nmos  (out, gnd, y);
+  
+endmodule
+
+// 2 input XOR gate 
+
+module xor2 (y,a,b);
+  output y;
+  input a,b;
+  supply0 gnd;
+  supply1 vdd;
+  wire w;
+  
+  pmos p1(w,vdd,a);
+  pmos p2(y,a,b);
+  pmos p3(y,b,a);
+  
+  nmos n1(w,gnd,a);
+  nmos n2(y,w,b);
+  nmos n3(y,b,w);
+	
+endmodule
+
+//--------
+
+module xor2 (y,a,b);
+  output y;
+  input a,b;
+  supply0 gnd;
+  supply1 vdd;
+  wire y1,y2,y3;
+  
+  nand n1(y1,a,b);
+  nand n2(y2,y1,a);
+  nand n3(y3,y1,b);
+  nand n4(y,y3,y2);
+  
+endmodule
+
+//----TB----
+
+module tb;
+  wire y; 
+ reg a,b;
+  
+  xor2 a1 ( y,a,b);
+  
+  initial begin
+    $dumpfile("dump.vcd");
+        $dumpvars(1);
+   #0; a =1; b=1;
+    #10; a=0; b=1;
+    #10; a=1; b=0;
+    #10; a=0; b=0;
+    #10;
+    
+  end
+  endmodule
+
+////------
+  
+  
+  
