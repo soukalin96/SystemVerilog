bdb22482ac194166b809ee4c068c6696de201b24
Soukalin Ghosh <97680241+soukalin96@users.noreply.github.com>
2022-01-15 16:03:26 +0530

    Update DFF.v

--- DFF.v
+++ DFF.v
@@ -1,50 +1,47 @@
 //DFF
+//logic
+module dff (input d,input clk,input res,output reg q,output qn);  
+  always @ (posedge clk or posedge res)  
+     if (res)  
+         q <= 0;  
+      else  
+         q <= d;  
+  assign qn = ~q;  
+endmodule  
 
-module dff(clk,rst,d,q,qb);
-//inputs
-input clk,rst,d;
-//output
-output q,qb;
-//register
-reg q;
-
-always @(posedge clk)
-begin
-if(rst)
-q=0;
-else
-q=d;
-end
-//q bar assignment
-assign qb=~q;
+//Gate netlist
+module dff (input d,input clk,input res,output reg q,output qn); 
+	wire  w1; wire  w2; wire  w3; wire  w4;
+  reg resn;
+  
+  not a1 (resn,res);
+	
+	nand n1 (w1,w4,w2);
+  nand n2 (w2,w1,clk,resn);
+	nand n3 (w3,clk,w2,w4);
+  nand n4 (w4,w3,d,resn);
+	nand n6 (q,w2,qn);
+  nand n5 (qn,q,w3,resn);
+	
 endmodule
 
-
+	
   //===== TB ======
   module test;
-      reg [2:0]a;
-      wire [1:0]out;
+      reg d; reg clk; reg res;
+      wire q; wire qn;
       
-      dff a1 (a[0],a[1],a[2],out[0],out[1]);
-      
-      
-      
+    dff a1 (d,clk,res,q,qn);
+      	  
       initial begin
          $dumpfile("dump.vcd");
         $dumpvars(1);
-        $monitor($time, " clk= %b reset =%b d= %b out = %b", a[0],a[1], a[2], out[0]);
-		
-	#0;          
-          a = 3'b010;
-        #20;   a[1] = 1'b0;a[0] = 1'b0;
-
-        forever #20 a[2] = ~ a[2];
+        $monitor($time, " clk= %b reset =%b d= %b out = %b", clk,d,res, q);          
+        #0; d=0; res = 1; clk = 0;
+	      #20;res = 1'b0; #20;res = 1'b1; #60; res = 1'b0;	  
+        forever #25 d = ~d;
       end 
-	 
-      always #10 a[0] = ~a[0];
-		initial #180 $stop;
-      
-
-endmodule  
-
+      always #10 clk = ~clk;
+      initial #200 $stop;
+  endmodule
 
