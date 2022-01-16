b0762911774455f33cd7bee4ebd580482c8f583f
Soukalin Ghosh <97680241+soukalin96@users.noreply.github.com>
2022-01-16 19:13:55 +0530

    Update DFF.v

--- DFF.v
+++ DFF.v
@@ -9,7 +9,7 @@ module dff (input d,clk,res,output reg q,qn);
   assign qn = ~q;  
 endmodule  
 
-//Gate netlist
+//Gate level
 module dff (input d,clk,res,output reg q, qn); 
 	wire  w1; wire  w2; wire  w3; wire  w4;
   reg resn;
@@ -24,6 +24,55 @@ module dff (input d,clk,res,output reg q, qn);
   nand n5 (qn,q,w3,resn);
 	
 endmodule
+
+//Switch Level
+
+module dff (input d,clk,output q, qn); 
+  
+  wire w1,w2,w3,w4;
+    supply0 gnd;
+	supply1 vdd;
+
+  tranif0 t1(d,w1,clk);
+
+  pmos p1(w2, vdd, w1);
+  nmos n1(w2, gnd, w1);
+  
+  pmos p2(w3, vdd, w2);
+  nmos n2(w3, gnd, w2);
+  
+  tranif0 t2(w3,w1,~clk);
+  
+  
+  tranif0 t3(w2,w4,~clk);
+  
+  pmos p3(q,vdd, w4);
+  nmos n3(q, gnd, w4);
+  
+  pmos p4(qn, vdd, q);
+  nmos n4(qn, gnd, q);
+  
+  tranif0 t4(qn,w4,clk);
+  
+endmodule
+
+/*   module test;
+      reg d; reg clk;
+      wire q; wire qn;
+      
+    dff a1 (d,clk,q,qn);
+      	  
+      initial begin
+         $dumpfile("dump.vcd");
+        $dumpvars(1);
+        $monitor($time, " clk= %b d= %b out = %b", clk,d,q);          
+        #0; d=1; clk = 0;
+        forever #25 d = ~d;
+      end 
+      always #10 clk = ~clk;
+      initial #200 $stop;
+  endmodule  */
+
 
 	
   //===== TB ======
